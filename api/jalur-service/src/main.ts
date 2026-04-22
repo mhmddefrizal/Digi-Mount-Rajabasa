f61908d7e93979c123416308476dfc800e552943
Muhammad Defrizal <muhammaddefrizal08@gmail.com>
2026-04-22 15:24:01 +0700

    perbaikan(main.ts): tambahkan prefix 'api' untuk endpoint global

--- api/jalur-service/src/main.ts
+++ api/jalur-service/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // tambahkan prefix 'api'
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();
