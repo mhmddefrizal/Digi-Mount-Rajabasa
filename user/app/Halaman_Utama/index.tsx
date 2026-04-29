bee6022bca1682fd1a6de714153058f32dfabbfd
Oka Windana Saputra <okwan2004@gmail.com>
2026-04-29 14:36:37 +0700

    update struktur

---
+++ user/app/Halaman_Utama/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function HalamanUtama() {
+  return (
+    <div>
+      <h1>Halaman Utama</h1>
+      <p>Selamat datang di halaman utama aplikasi!</p>
+    </div>
+  );
+}
