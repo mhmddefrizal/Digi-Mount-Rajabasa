536c08d73b770a2fcddbde53c2b0c2f9d47bcafd
Oka Windana Saputra <okwan2004@gmail.com>
2026-04-29 14:44:44 +0700

    update erorr

--- user/app/login.tsx
+++ user/app/login.tsx
@@ -1,3 +1,11 @@
+import { Text } from "@react-navigation/elements";
 import React from "react";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 export default function Login() {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
