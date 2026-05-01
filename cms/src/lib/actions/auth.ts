5c4b3b01c5c7edf4265104cda9cbdae319826822
Rivaldzz <rivaldosborn@gmail.com>
2026-05-01 12:52:39 +0700

    validasi sederhana untuk memastikan username dan password tidak kosong

--- cms/src/lib/actions/auth.ts
+++ cms/src/lib/actions/auth.ts
@@ -1,4 +1,15 @@
 import { AuthResponse, LoginFormData } from "@/types/auth";
 
+// pembuatan fungsi untuk melakukan login, menerima data login dan mengembalikan response dari server
+export async function loginAction(data: LoginFormData): Promise<AuthResponse> {
 
-export async function loginAction(data: LoginFormData): Promise<AuthResponse> {}
+    const { username, password } = data;
+
+    // validasi sederhana untuk memastikan username dan password tidak kosong
+    if (!username || !password) {
+        return {
+            success: false,
+            message: "Username and password are required.",
+        };
+    }
+}
