5ece75b2673a2eef66f1829e92da756136955ba2
Rivaldzz <rivaldosborn@gmail.com>
2026-05-01 12:45:51 +0700

    pembuatan interface untuk data response login

--- cms/src/types/auth.ts
+++ cms/src/types/auth.ts
@@ -4,3 +4,11 @@ export interface LoginFormData {
     password: string;
     keepSession: boolean;
 }
+
+// pembuatan interface untuk data response login
+export interface AuthResponse {
+    success: boolean;
+    message: string;
+    token?: string; // token opsional, hanya ada jika login berhasil
+    redirectTo?: string; // URL untuk redirect setelah login berhasil, opsional
+}
