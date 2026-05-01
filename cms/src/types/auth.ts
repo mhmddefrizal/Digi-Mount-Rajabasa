de55811f91851f71f7775ab005130dadc1602e99
Rivaldzz <rivaldosborn@gmail.com>
2026-05-01 12:47:00 +0700

    pembuatan interface untuk data statistik yang akan ditampilkan di dashboard

--- cms/src/types/auth.ts
+++ cms/src/types/auth.ts
@@ -12,3 +12,9 @@ export interface AuthResponse {
     token?: string; // token opsional, hanya ada jika login berhasil
     redirectTo?: string; // URL untuk redirect setelah login berhasil, opsional
 }
+
+// pembuatan interface untuk data statistik yang akan ditampilkan di dashboard
+export interface StatItem {
+    label: string;
+    value: string;
+}
