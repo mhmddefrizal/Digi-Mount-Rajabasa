0830eb332a8425d20ffc187921f42373317eefb1
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-02 16:37:38 +0700

    Fitur: Perbarui konfigurasi NestJS dan Prisma, termasuk pengaturan assets dan pengecualian direktori

--- api/jalur/src/prisma.service.ts
+++ api/jalur/src/prisma.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaClient } from './generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 @Injectable()
 export class PrismaService extends PrismaClient {
