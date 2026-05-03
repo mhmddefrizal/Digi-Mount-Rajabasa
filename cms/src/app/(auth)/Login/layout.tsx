f79ee6e48f8e7eccf85ab98a814e1c2e70c4b823
Rivaldzz <rivaldosborn@gmail.com>
2026-05-03 16:19:49 +0700

    pembagian panel menjadi 2

--- cms/src/app/(auth)/Login/layout.tsx
+++ cms/src/app/(auth)/Login/layout.tsx
@@ -1,17 +1,21 @@
-
 // halaman layout untuk halaman login
 // import komponen auth hero
 import AuthHero from "@/components/layout/AuthHero";
 export default function LoginLayout({
-    // penambahan properti children untuk menampilkan konten halaman login di sebelah kanan hero
-    children,
+  // penambahan properti children untuk menampilkan konten halaman login di sebelah kanan hero
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <div className="flex min-h-screen">
-            <AuthHero />
-            <div className="flex flex-1">{children}</div>
-        </div>
-    );
+  return (
+    // bagian utama yang membagi layar menjadi dua, bagian kiri untuk hero dan bagian kanan untuk form login
+    <main className="flex min-h-screen">
+      <AuthHero />
+
+      {/* Right Panel */}
+      <section className="flex w-full lg:w-1/2 items-center justify-center bg-panel px-6">
+        {children}
+      </section>
+    </main>
+  );
 }
