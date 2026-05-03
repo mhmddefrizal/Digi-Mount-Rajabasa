ca4072451fd5fd5d49c1c53d140cfcd5b0c89669
Rivaldzz <rivaldosborn@gmail.com>
2026-05-03 15:45:06 +0700

    berikan Untuk input email

--- cms/src/components/auth/LoginForm.tsx
+++ cms/src/components/auth/LoginForm.tsx
@@ -1,8 +1,21 @@
 "use client";
 
+import { Input } from "../ui/input";
+
 // komponen form login yang akan digunakan di halaman login
 export default function LoginForm() {
   return (
-    <form className="w-full max-w-md space-y-5"></form>
+    <form className="w-full max-w-md space-y-5">
+      {/* Untuk input email */}
+      <div className="space-y-2">
+        <label className="text-sm font-medium text-text-secondary uppercase tracking-wide">
+          Operator Email
+        </label>
+        <Input
+          placeholder="example@gmail.com"
+          className="bg-input border-border focus-visible:ring-primary"
+        />
+      </div>
+    </form>
   );
 }
