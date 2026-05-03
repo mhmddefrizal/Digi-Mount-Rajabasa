696504c87ff3e3a396e95734deeea2c6cc8a2167
Rivaldzz <rivaldosborn@gmail.com>
2026-05-03 16:23:32 +0700

    halaman login yang menampilkan form login

---
+++ cms/src/app/(auth)/Login/page.tsx
@@ -0,0 +1,8 @@
+// halaman login yang menampilkan form login
+import LoginForm from "@/components/auth/LoginForm";
+
+export default function LoginPage() {
+    return (
+        <LoginForm />
+    );
+}
