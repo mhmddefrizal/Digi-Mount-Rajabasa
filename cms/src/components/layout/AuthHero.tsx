9eb85048d94d2c23dc7559d252b9263ebcffc6e6
Rivaldzz <rivaldosborn@gmail.com>
2026-05-03 15:56:21 +0700

    tambah komponen untuk menampilkan bagian hero pada halaman login dan register

---
+++ cms/src/components/layout/AuthHero.tsx
@@ -0,0 +1,6 @@
+// komponen ini digunakan untuk menampilkan bagian hero pada halaman login dan register
+export default function AuthHero() {
+    return (
+      <section className="relative hidden lg:flex w-1/2 h-screen text-white"></section>
+    );
+}
