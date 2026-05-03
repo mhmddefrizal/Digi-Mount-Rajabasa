7f89ac012954772772aadc32977e71b59d7690ba
Rivaldzz <rivaldosborn@gmail.com>
2026-05-03 16:18:59 +0700

    penambahan properti children untuk menampilkan konten halaman login di sebelah kanan hero

--- cms/src/app/(auth)/Login/layout.tsx
+++ cms/src/app/(auth)/Login/layout.tsx
@@ -2,4 +2,16 @@
 // halaman layout untuk halaman login
 // import komponen auth hero
 import AuthHero from "@/components/layout/AuthHero";
-export default function LoginLayout({})
+export default function LoginLayout({
+    // penambahan properti children untuk menampilkan konten halaman login di sebelah kanan hero
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="flex min-h-screen">
+            <AuthHero />
+            <div className="flex flex-1">{children}</div>
+        </div>
+    );
+}
