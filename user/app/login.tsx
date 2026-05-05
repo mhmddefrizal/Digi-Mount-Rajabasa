d64ca24f6721ec1f306fc5705483f02553478f26
Oka Windana Saputra <okwan2004@gmail.com>
2026-05-05 14:19:48 +0700

    panggil fungsi style container

--- user/app/login.tsx
+++ user/app/login.tsx
@@ -7,7 +7,7 @@ export default function Login() {
    const [password, setPassword] = useState("");
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login</Text>
     </View>
   );
