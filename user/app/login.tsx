adc3e808d5a3001698d6da7236d0781a4579cc67
Oka Windana Saputra <okwan2004@gmail.com>
2026-05-05 13:47:43 +0700

    menambahkan state username

--- user/app/login.tsx
+++ user/app/login.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text } from "react-native";
 
 export default function Login() {
+   const [username, setUsername] = useState("");
+
   return (
     <View>
       <Text>Login</Text>
