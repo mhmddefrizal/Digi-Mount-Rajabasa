1298e694b0da6fea97b67186a994dab4fa0bffd0
Muhammad Defrizal <muhammaddefrizal08@gmail.com>
2026-05-06 15:13:50 +0700

    feat(gateway): menginisialisasi aplikasi NestJS dengan struktur dasar dan pengujian
    - Tambahkan package.json dengan dependensi dan skrip untuk membangun, menguji, dan melakukan linting.
    
    - Buat AppController dan AppService dengan endpoint sederhana "Hello World!".
    
    - Implementasikan pengujian unit untuk AppController.
    
    - Siapkan pengujian end-to-end untuk aplikasi.
    
    - Tambahkan file konfigurasi TypeScript untuk membangun dan pengembangan.
    
    - Tetapkan konfigurasi Jest untuk pengujian unit dan e2e.

---
+++ api/gateway/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // buat prefix api
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT ?? 3003);
+}
+void bootstrap();
