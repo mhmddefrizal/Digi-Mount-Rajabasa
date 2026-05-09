2daf60f10bb4fc85728aed0be311abc646c06569
Rivaldzz <rivaldosborn@gmail.com>
2026-05-09 14:03:28 +0700

    add sidebar for dashboard

---
+++ cms/src/components/dashboard/dashboard-sidebar.tsx
@@ -0,0 +1,11 @@
+import {
+  CreditCard,
+  LayoutDashboard,
+  Map,
+  ClipboardList,
+} from "lucide-react";
+
+// TODO: add sidebar for dashboard
+export default function DashboardSidebar() {
+  return ();
+}
