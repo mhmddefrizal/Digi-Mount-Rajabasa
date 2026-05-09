e7917bc55de61dd8aadc237963bbf93f0df5b602
Rivaldzz <rivaldosborn@gmail.com>
2026-05-09 20:24:51 +0700

    buat function yang akan digunakan keseluruhan componen dashboard

--- cms/src/app/(dashboard)/dashboard/layout.tsx
+++ cms/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,1 +1,15 @@
 // layout dashboard
+import DashboardLayout from "@/components/layout/dashboard-layout";
+
+// buat layout untuk dashboard, yang akan digunakan untuk semua halaman di dalam dashboard
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <DashboardLayout>
+      {children}
+    </DashboardLayout>
+  );
+}
