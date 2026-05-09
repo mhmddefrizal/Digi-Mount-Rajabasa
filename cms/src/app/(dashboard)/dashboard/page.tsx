8f2d995f4e6596adc610361c3c785dd1d6b2d70b
Rivaldzz <rivaldosborn@gmail.com>
2026-05-09 20:29:26 +0700

    tampilan dashboard (overview 1)

--- cms/src/app/(dashboard)/dashboard/page.tsx
+++ cms/src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,12 @@
 
 // page halaman dashboard
 export default function DashboardPage() {
-    return ()
+    return (
+        // tampilan dashboard overview yang menampilkan beberapa informasi penting seperti jumlah ekspedisi, jumlah barang, dll
+        <main className="space-y-8">
+            <h1 className="font-display text-6xl leading-none">
+                Expedition Overview
+            </h1>
+        </main>
+    )
 }
