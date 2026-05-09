ecce748118d6cbe3f622030d1df3d8b353c805cd
Rivaldzz <rivaldosborn@gmail.com>
2026-05-09 20:39:30 +0700

    tambah icon untuk stats card

---
+++ cms/src/components/dashboard/statscard.tsx
@@ -0,0 +1,18 @@
+
+
+interface Props {
+  title: string;
+  value: string;
+  description: string;
+  color: string;
+}
+
+// TODO: tambah icon untuk stats card
+export default function StatsCard({
+  title,
+  value,
+  description,
+  color,
+}: Props) {
+  return ()
+}
