ed2517b669d020cbeb39ed0f54e6e8504e53721f
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-09 22:35:12 +0700

    feat(jalur): mengimplementasikan interceptor respons untuk penanganan kesalahan di jalur_api

--- api/gateway/src/common/instances/jalur.instance.ts
+++ api/gateway/src/common/instances/jalur.instance.ts
@@ -1,5 +1,24 @@
-import axios from 'axios';
+import { HttpException } from '@nestjs/common';
+import axios, { AxiosError } from 'axios';
 
 export const jalur_api = axios.create({
   baseURL: 'http://localhost:3001/api/jalur',
 });
+
+// interceptor response
+jalur_api.interceptors.response.use(
+  (response) => response,
+
+  (error: AxiosError) => {
+    const message = error.response?.data;
+    const status = error.response?.status;
+
+    // kalau error dari service terbaca
+    if (status && message) {
+      throw new HttpException(message, status);
+    }
+
+    // fallback error
+    throw new HttpException('Internal Server Error', 500);
+  },
+);
