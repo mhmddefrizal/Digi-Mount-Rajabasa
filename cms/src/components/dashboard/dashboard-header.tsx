63ad1f38a26b0a38e7ab18f7fa64b53f3c725217
Rivaldzz <rivaldosborn@gmail.com>
2026-05-09 16:33:58 +0700

    tambah dashboard header bagian search

--- cms/src/components/dashboard/dashboard-header.tsx
+++ cms/src/components/dashboard/dashboard-header.tsx
@@ -1,5 +1,19 @@
+import { Search } from "lucide-react";
+import { Input } from "../ui/input";
 
 // dashboard header
 export default function DashboardHeader() {
-    return ()
+  return (
+    <header className="h-20 border-b border-border bg-background px-8 flex items-center justify-between">
+      {/* bagian search */}
+      <section className="relative w-105">
+        <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-text-muted" />
+
+        <Input
+          placeholder="Search registrations, climbers, or permits..."
+          className="pl-10 h-11 bg-panel border-border"
+        />
+      </section>
+    </header>
+  );
 }
