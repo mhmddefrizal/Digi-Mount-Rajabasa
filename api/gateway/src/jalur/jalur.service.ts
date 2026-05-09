d9be48dcc2a068b3233209af7686f8128d1a0d22
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-09 16:32:02 +0700

    feat(jalur): mengimplementasikan metode create di JalurService dengan integrasi API

--- api/gateway/src/jalur/jalur.service.ts
+++ api/gateway/src/jalur/jalur.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { jalur_api } from '../common/instances/jalur.instance';
+
 import { CreateJalurDto } from './dto/create-jalur.dto';
 import { UpdateJalurDto } from './dto/update-jalur.dto';
 
+// buat interface untuk data kategori
+export interface Jalur {
+  id: string;
+  nama: string;
+  deskripsi?: string;
+  tingkatKesulitan: string;
+  createdAt: Date;
+}
 @Injectable()
 export class JalurService {
-  create(createJalurDto: CreateJalurDto) {
-    return 'This action adds a new jalur';
+  async create(createJalurDto: CreateJalurDto) {
+    // return 'This action adds a new jalur';
+    const response = await jalur_api.post('/', createJalurDto);
+    return response.data;
   }
 
   findAll() {
