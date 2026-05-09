4c5c3a55ba331067778eb7e9fcd1dbb3845f5680
Rivaldzz <rivaldosborn@gmail.com>
2026-05-09 20:36:05 +0700

    tambah section untuk stats dan content

--- cms/src/app/(dashboard)/dashboard/page.tsx
+++ cms/src/app/(dashboard)/dashboard/page.tsx
@@ -3,7 +3,7 @@ export default function DashboardPage() {
    return (
       // tampilan dashboard overview yang menampilkan beberapa informasi penting seperti jumlah ekspedisi, jumlah barang, dll
       <main className="space-y-8">
-        {/* bungkus dengan header */}
+         {/* bungkus dengan header */}
          <header>
             <h1 className="font-display text-6xl leading-none">
                Expedition Overview
@@ -12,6 +12,16 @@ export default function DashboardPage() {
                Real-time logistics and climber activity monitoring.
             </p>
          </header>
+
+         {/* stats */}
+         <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5"></section>
+
+         {/* Content */}
+         <section className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+            <article className="xl:col-span-2">
+                {/* <TrafficChart /> */}
+            </article>
+         </section>
       </main>
    );
 }
