e5187b63bc1e4b8a6302026913a150ccac85495b
Rivaldzz <rivaldosborn@gmail.com>
2026-05-09 20:15:33 +0700

    add a loading state for the dashboard layout

---
+++ cms/src/components/layout/dashboard-layout.tsx
@@ -0,0 +1,10 @@
+import DashboardHeader from "../dashboard/dashboard-header";
+import DashboardSidebar from "../dashboard/dashboard-sidebar";
+
+// TODO: add a loading state for the dashboard layout
+export default function DashboardLayout({
+  children,
+}: Props) {
+  return (
+  )
+}
