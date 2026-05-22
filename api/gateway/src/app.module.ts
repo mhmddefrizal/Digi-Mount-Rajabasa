84c2973446c4ec069663d6a1459d66c23377a541
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-23 00:07:11 +0700

    feat(auth): menambahkan AuthModule untuk menangani logika otentikasi

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JalurModule } from './jalur/jalur.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [JalurModule],
+  imports: [JalurModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
