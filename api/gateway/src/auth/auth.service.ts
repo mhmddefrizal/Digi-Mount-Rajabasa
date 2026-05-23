78c9b60c4955d21b6042d7dc2faa1677a5c98c94
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-23 22:34:39 +0700

    feat(auth): merefaktor logika token penyegaran untuk menggunakan input pengguna dari AuthDto

--- api/gateway/src/auth/auth.service.ts
+++ api/gateway/src/auth/auth.service.ts
@@ -23,31 +23,33 @@ export class AuthService {
 
     // jika email dan password valid, buat token JWT
     // buat payload untuk JWT
-    const payload = {
-      email: dto.email,
-      password: dto.password,
-    };
+    refresh(user: AuthDto) {
+      const payload = {
+        email: user.email,
+        password: user.password,
+      };
 
-    // tampilkan hasil respon
-    return {
-      success: true,
-      message: 'Access token berhasil dibuat !',
+      // tampilkan hasil respon
+      return {
+        success: true,
+        message: 'Access token berhasil dibuat !',
 
-      metadata: {
-        status: HttpStatus.CREATED,
-      },
+        metadata: {
+          status: HttpStatus.CREATED,
+        },
 
-      // tampilkan data access token dan refresh token
-      data: {
-        access_token: this.jwtService.sign(payload, {
-          secret: 'access-gunung-rajabasa-oke',
-          expiresIn: '15m',
-        }),
-        refresh_token: this.jwtService.sign(payload, {
-          secret: 'refresh-gunung-rajabasa-oke',
-          expiresIn: '7d',
-        }),
-      },
-    };
+        // tampilkan data access token dan refresh token
+        data: {
+          access_token: this.jwtService.sign(payload, {
+            secret: 'access-gunung-rajabasa-oke',
+            expiresIn: '15m',
+          }),
+          refresh_token: this.jwtService.sign(payload, {
+            secret: 'refresh-gunung-rajabasa-oke',
+            expiresIn: '7d',
+          }),
+        },
+      };
+    }
   }
 }
