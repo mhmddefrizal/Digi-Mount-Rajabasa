0bbfdd7db0302840269bb54a485cc095f8dd24b9
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-23 23:25:54 +0700

    feat(auth): tambahkan pengecekan header 'x-internal-secret' di InternalGuard untuk otorisasi internal

--- api/jalur/src/internal.guard.ts
+++ api/jalur/src/internal.guard.ts
@@ -13,6 +13,14 @@ export class InternalGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<Request>();
 
+    // tambahkan pengecekan header 'x-internal-secret' untuk memastikan bahwa request berasal dari internal
     const secret = request.headers['x-internal-secret'];
+
+    if (secret !== 'rahasia-banget-cik') {
+      throw new UnauthorizedException({
+        statusCode: HttpStatus.UNAUTHORIZED,
+        message: 'Unauthorized',
+      });
+    }
   }
 }
