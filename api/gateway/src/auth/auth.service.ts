6e3cc81efcdf303a632fb342103d97bd4da641d0
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-23 14:56:49 +0700

    feat(auth): menambahkan fungsi login dengan validasi pengguna statis di AuthService

--- api/gateway/src/auth/auth.service.ts
+++ api/gateway/src/auth/auth.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
   // buat constructor untuk inject JwtService
   constructor(private jwtService: JwtService) {}
+
+  // buat fungsi login
+  login(dto: AuthDto) {
+    // validasi static user
+    if (dto.email !== 'admin@gmail.com' || dto.password !== 'admin123') {
+      throw new UnauthorizedException({
+        success: false,
+        message: 'Email atau password tidak valid !',
+      });
+    }
+  }
 }
