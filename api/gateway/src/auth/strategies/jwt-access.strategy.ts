4a835a189601094b2fc88123500f4d2bee14dc4c
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-23 20:03:09 +0700

    feat(auth): menambahkan akses JWT dan memperbarui guard dan strategi

---
+++ api/gateway/src/auth/strategies/jwt-access.strategy.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import * as passportJwt from 'passport-jwt';
+
+const { ExtractJwt, Strategy } = passportJwt;
+
+// interface payload
+interface JwtPayload {
+  email: string;
+  password: string;
+}
