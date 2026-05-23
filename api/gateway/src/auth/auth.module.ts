7bdb9ec47b0a656142e00885c394257579e706cb
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-23 22:19:36 +0700

    feat(auth): menambahkan JwtAccessStrategy dan JwtRefreshStrategy ke penyedia AuthModule

--- api/gateway/src/auth/auth.module.ts
+++ api/gateway/src/auth/auth.module.ts
@@ -5,12 +5,14 @@ import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
+import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
 
 @Module({
   imports: [PassportModule, JwtModule.register({})],
 
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtAccessStrategy, JwtRefreshStrategy],
 
   exports: [PassportModule, JwtModule],
 })
