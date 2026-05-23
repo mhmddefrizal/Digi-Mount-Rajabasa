b49bf6578e22fcd50b25818af957aefb4549ba19
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-23 23:31:19 +0700

    perbaikan(utama): tentukan nama host 'localhost' di app.listen untuk kejelasan

--- api/jalur/src/main.ts
+++ api/jalur/src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // tambahkan prefix 'api'
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT!);
+  await app.listen(process.env.PORT!, 'localhost');
 }
 void bootstrap();
