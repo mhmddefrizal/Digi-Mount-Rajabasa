ec6faa8858b9b9dc078fecbb3c9a4f586485980d
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-05-23 14:07:27 +0700

    feat(auth): menambahkan impor JwtModule dan PassportModule ke AuthModule

--- api/gateway/src/auth/auth.module.ts
+++ api/gateway/src/auth/auth.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
