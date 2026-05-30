b6251ac2356b37a4e9c662c5fc966730a149b3c1
Rivaldzz <rivaldosborn@gmail.com>
2026-05-30 19:29:33 +0700

    tombol untuk melihat semua trail

--- cms/src/components/registrations/registration-filters.tsx
+++ cms/src/components/registrations/registration-filters.tsx
@@ -1,8 +1,19 @@
-
+import { Button } from "@/components/ui/button";
 
 // tambah filter untuk registration, seperti filter berdasarkan tanggal, status, dll.
 export default function RegistrationFilters() {
-    return (
-        <div>RegistrationFilters</div>
-    )
+   return (
+    // section untuk bagian filter
+      <section className="flex flex-wrap items-center justify-between gap-4">
+         <div className="flex gap-3">
+            {/* tombol untuk melihat semua trail */}
+            <Button 
+                variant="outline"
+                className="min-w-45"
+            >
+                All Trails
+            </Button>
+         </div>
+      </section>
+   );
 }
