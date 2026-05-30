2798794bc148c92f27b3c12f68f26bc87f5cf9f7
Rivaldzz <rivaldosborn@gmail.com>
2026-05-30 19:26:06 +0700

    tambah filter untuk registration, seperti filter berdasarkan tanggal, status, dll.

---
+++ cms/src/components/registrations/registration-filters.tsx
@@ -0,0 +1,8 @@
+
+
+// tambah filter untuk registration, seperti filter berdasarkan tanggal, status, dll.
+export default function RegistrationFilters() {
+    return (
+        <div>RegistrationFilters</div>
+    )
+}
