14c2a42c2be39149accb6142acb72ec0b2924ec2
Oka Windana Saputra <okwan2004@gmail.com>
2026-05-30 08:13:52 +0700

    membuat dan menambahkan style container untuk regis

---
+++ user/styles/RegisterStyle.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EEF2EC",
+    alignItems: "center",
+    padding: 20,
+  },
