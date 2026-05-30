e4cdbb967763e53762a2f39ef6b81e02c9110068
Oka Windana Saputra <okwan2004@gmail.com>
2026-05-30 08:45:13 +0700

    menambahkan state nama dan agree

--- user/app/(auth)/register.tsx
+++ user/app/(auth)/register.tsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import styles from "@/styles/RegisterStyle";
 import Ionicons from "@expo/vector-icons/build/Ionicons";
-import Ionicons from "@expo/vector-icons/build/Ionicons";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [nama, setNama] = useState("");
+  const [agree, setAgree] = useState(false);
 
   return (
     <View style={styles.container}>
