54454d4b7d17ddd18b09e758396a55b61d1f2923
Rivaldzz <rivaldosborn@gmail.com>
2026-05-30 19:53:57 +0700

    bagian utama untuk menampilkan daftar pendaftaran

--- cms/src/app/(dashboard)/registrations/page.tsx
+++ cms/src/app/(dashboard)/registrations/page.tsx
@@ -2,8 +2,14 @@
 // halaman untuk menampilkan daftar pendaftaran
 export default function RegistrationPage() {
     return (
-        <div className="space-y-5">
-            
-        </div>
+        // bagian utama untuk menampilkan daftar pendaftaran
+        <main className="space-y-6">
+            {/* bagian untuk menampilkan header */}
+            <header>
+                <h1 className="font-display text-6xl leading-none">
+                    Registration List
+                </h1>
+            </header>
+        </main>
     );
 }
