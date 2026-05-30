d303badc026a2a58fe9231a16d23a97ff6d8e64b
Rivaldzz <rivaldosborn@gmail.com>
2026-05-30 19:52:58 +0700

    halaman untuk menampilkan daftar pendaftaran

---
+++ cms/src/app/(dashboard)/registrations/page.tsx
@@ -0,0 +1,9 @@
+
+// halaman untuk menampilkan daftar pendaftaran
+export default function RegistrationPage() {
+    return (
+        <div className="space-y-5">
+            
+        </div>
+    );
+}
