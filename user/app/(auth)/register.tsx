c0fbac307da263c423660735f5ea779afd8d7c92
Oka Windana Saputra <okwan2004@gmail.com>
2026-05-30 08:24:20 +0700

    update tampilan register dengan icon

--- user/app/(auth)/register.tsx
+++ user/app/(auth)/register.tsx
@@ -6,8 +6,18 @@ import {
   TouchableOpacity,
 } from "react-native";
 import styles from "@/styles/RegisterStyle";
+import Ionicons from "@expo/vector-icons/build/Ionicons";
+import Ionicons from "@expo/vector-icons/build/Ionicons";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  
+ return (
+    <View style={styles.container}>
+
+      {/* Header Icon */}
+      <View style={styles.iconContainer}>
+        <Ionicons name="walk" size={40} color="white" />
+      </View>
