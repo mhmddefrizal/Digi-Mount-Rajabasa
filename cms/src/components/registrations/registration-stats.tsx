527456d96d83fd18110deef2b74fdde1c7ba6d1f
Rivaldzz <rivaldosborn@gmail.com>
2026-05-30 19:37:12 +0700

    card untuk menampilkan data pendaftaran

--- cms/src/components/registrations/registration-stats.tsx
+++ cms/src/components/registrations/registration-stats.tsx
@@ -1,6 +1,24 @@
-
+import StatsCard from "@/components/dashboard/stats-card";
 
 // fungsi untuk menampilkan statistik pendaftaran
 export default function RegistrationStats() {
-    return <div>RegistrationStats</div>
+   return (
+      <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
+         {/* card untuk menampilkan data pendaftaran */}
+         {/* masih menggunakan data dummy */}
+         <StatsCard
+            title="TOTAL PENDING"
+            value="124"
+            description="+12%"
+            color="text-primary"
+         />
+         <StatsCard
+            title="VERIFIED TODAY"
+            value="42"
+            description="Stable"
+            color="text-green-600"
+         />
+
+      </section>
+   );
 }
