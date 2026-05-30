88db9223203f219e2f7c839033deefc1a9e57b58
Rivaldzz <rivaldosborn@gmail.com>
2026-05-30 19:33:06 +0700

    tambah tombol untuk menambah registration

--- cms/src/components/registrations/registration-filters.tsx
+++ cms/src/components/registrations/registration-filters.tsx
@@ -3,24 +3,22 @@ import { Button } from "@/components/ui/button";
 // tambah filter untuk registration, seperti filter berdasarkan tanggal, status, dll.
 export default function RegistrationFilters() {
    return (
-    // section untuk bagian filter
+      // section untuk bagian filter
       <section className="flex flex-wrap items-center justify-between gap-4">
          <div className="flex gap-3">
             {/* tombol untuk melihat semua trail */}
-            <Button 
-                variant="outline"
-                className="min-w-45"
-            >
-                All Trails
+            <Button variant="outline" className="min-w-45">
+               All Trails
             </Button>
             {/* tombol untuk melihat semua status */}
-            <Button 
-                variant="outline"
-                className="min-w-45"
-            >
-                All Status
+            <Button variant="outline" className="min-w-45">
+               All Status
             </Button>
          </div>
+         {/* tombol untuk menambah registration */}
+         <Button className=" bg-primary hover:bg-primary-light text-white ">
+            + New Registration
+         </Button>
       </section>
    );
 }
