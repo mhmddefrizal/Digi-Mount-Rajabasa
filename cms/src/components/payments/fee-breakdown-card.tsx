8f7cb6149a7204b2d971e385dc17a582d0227dd0
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 21:08:13 +0700

    komponen FeeBreakdownCard untuk menampilkan rincian biaya lainnya pembayaran

---
+++ cms/src/components/payments/fee-breakdown-card.tsx
@@ -0,0 +1,9 @@
+import { Card, CardHeader, CardTitle } from "../ui/card";
+
+// komponen FeeBreakdownCard untuk menampilkan rincian biaya lainnya pembayaran
+<Card>
+  <CardHeader>
+    <CardTitle>Fee Breakdown</CardTitle>
+  </CardHeader>
+    {/* card rincian biaya pembayaran */}
+</Card>
