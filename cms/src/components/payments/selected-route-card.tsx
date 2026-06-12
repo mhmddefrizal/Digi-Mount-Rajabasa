f744d765c8918dc2334b91fc3dcaef3eb9cdd26f
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 21:11:56 +0700

    tambah komponen SelectedRouteCard untuk menampilkan informasi jalur yang dipilih

---
+++ cms/src/components/payments/selected-route-card.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader, CardTitle } from "../ui/card";
+
+// komponen SelectedRouteCard untuk menampilkan informasi jalur yang dipilih
+<Card>
+
+  <CardHeader>
+    <CardTitle>
+      Selected Route
+    </CardTitle>
+  </CardHeader>
+</Card>
