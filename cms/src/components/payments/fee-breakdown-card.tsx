bfb9fac5c7df1384db2d82f7091091bfe3b463ff
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 21:10:45 +0700

    isi kartu rincian biaya (masih data dummy)

--- cms/src/components/payments/fee-breakdown-card.tsx
+++ cms/src/components/payments/fee-breakdown-card.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 // komponen FeeBreakdownCard untuk menampilkan rincian biaya lainnya pembayaran
 <Card>
@@ -6,4 +6,26 @@ import { Card, CardHeader, CardTitle } from "../ui/card";
     <CardTitle>Fee Breakdown</CardTitle>
   </CardHeader>
     {/* card rincian biaya pembayaran */}
+
+    {/* isi kartu rincian biaya (masih data dummy) */}
+    <CardContent>
+        <div className="space-y-5">
+            <div className="flex justify-between">
+                <span>Climber Fees</span>
+                <span>Rp 750.000</span> {/* biaya pendaki (dummy) */}
+            </div>
+            <div className="flex justify-between">
+                <span>Guide Fees</span>
+                <span>Rp 175.000</span> {/* biaya pemandu (dummy) */}
+            </div>
+            <div className="flex justify-between">
+                <span>Parkir Kendaraan</span>
+                <span>Rp 25.000</span> {/* biaya parkir (dummy) */}
+            </div>
+            <div className="flex justify-between">
+                <span>SIMAKSI Permit</span>
+                <span>Rp 155.000</span> {/* biaya SIMAKSI (dummy) */}
+            </div>
+        </div>
+    </CardContent>
 </Card>
