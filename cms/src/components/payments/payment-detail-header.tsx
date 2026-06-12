8331782cc6f3fa0e75bbf64e0d569516df82220c
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 20:53:51 +0700

    tambah header dengan judul dan status pembayaran

--- cms/src/components/payments/payment-detail-header.tsx
+++ cms/src/components/payments/payment-detail-header.tsx
@@ -1,5 +1,21 @@
+import { Badge } from "../ui/badge";
 
 // komponen PaymentDetailHeader untuk menampilkan header detail pembayaran
 export default function PaymentDetailHeader() {
-    return ()
+    return (
+        // header dengan judul dan status pembayaran
+        <header className="flex items-start justify-between">
+            <article>
+                <div className="flex items-center gap-3">
+                    <h1 className="font-display text-5xl">
+                        {/* Transaction ID (Dummy) */}
+                        #TRX-8842  
+                    </h1>
+                    <Badge className="bg-green-100 text-green-700">
+                        Paid
+                    </Badge>
+                </div>
+            </article>
+        </header>
+    )
 }
