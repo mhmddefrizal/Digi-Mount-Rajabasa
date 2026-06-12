ac0c8b234921d2a718b667b2cbd6ec1c8ca6b28d
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 20:21:57 +0700

    tambah komponen tabel payment

--- cms/src/components/payments/payment-table.tsx
+++ cms/src/components/payments/payment-table.tsx
@@ -1,3 +1,16 @@
+import { Card, CardHeader, CardTitle } from "../ui/card";
+
+// komponen PaymentTable untuk menampilkan tabel pembayaran
 export default function PaymentTable() {
-    return ()
+    return (
+        <section>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between">
+                    <CardTitle>
+                        Expedition Groups
+                    </CardTitle>
+                </CardHeader>
+            </Card>
+        </section>
+    )
 }
