61a86661106a237229e7d860014f16ddc15e4b25
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 19:11:53 +0700

    halaman untuk menampilkan daftar trail

--- cms/src/app/(dashboard)/trails/page.tsx
+++ cms/src/app/(dashboard)/trails/page.tsx
@@ -1,3 +1,13 @@
+import TrailGrid from "@/components/trails/trail-grid";
+
+// halaman untuk menampilkan daftar trail
 export default function TrailsPage() {
-    return ()
+    return (
+        <main className="space-y-8">
+            <header className="flex items-center justify-between">
+
+            </header>
+            <TrailGrid />
+        </main>
+    )
 };
