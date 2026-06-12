3334fcc10f1176fc15675f446ae0b6e88727b667
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 21:17:16 +0700

    add layout utama halaman detail pembayaran

--- cms/src/app/(dashboard)/payments/[id]/page.tsx
+++ cms/src/app/(dashboard)/payments/[id]/page.tsx
@@ -1,5 +1,17 @@
+import CustomerInformationCard from "@/components/payments/customer-information-card";
+import PaymentDetailHeader from "@/components/payments/payment-detail-header";
 
 // halaman detail pembayaran
 export default function PaymentDetailPage() {
-  return ()
+  return (
+    // layout utama halaman detail pembayaran
+    <main className="space-y-6">
+        <PaymentDetailHeader />
+        <section className="grid grid-cols-12 gap-6">
+            <article className="col-span-8">
+                <CustomerInformationCard />
+            </article>
+        </section>
+    </main>
+  )
 }
