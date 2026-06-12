110cf3018046592dcda0c73576119bd057327bbc
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 20:44:42 +0700

    layout utama halaman pembayaran

--- cms/src/app/(dashboard)/payments/page.tsx
+++ cms/src/app/(dashboard)/payments/page.tsx
@@ -1,5 +1,16 @@
+import PaymentStats from "@/components/payments/payment-stats";
+import PaymentTable from "@/components/payments/payment-table";
 
 // function halaman pembayaran
 export default function PaymentsPage() {
-    return ()
+    return (
+        // layout utama halaman pembayaran dengan spasi antar elemen
+        <main className="space-y-8">
+            <header className="flex items-start justify-between">
+
+            </header>
+            <PaymentStats />
+            <PaymentTable />
+        </main>
+    )
 }
