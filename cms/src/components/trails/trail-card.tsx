2c35ad37155a48719f3f7b0dc905ad0cc0f5ca67
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 18:46:56 +0700

    add file trail-card.tsx

---
+++ cms/src/components/trails/trail-card.tsx
@@ -0,0 +1,10 @@
+// import packages gambar 
+import Image from "next/image";
+
+// import components kartu
+import { Card, CardContent } from "@components/ui/card";
+
+// import komponen badge, switch, dan ikon
+import { Badge } from "@components/ui/badge";
+import { Switch } from "@components/ui/switch";
+import { MapPin, Pencil, Trash2 } from "lucide-react";
