23a650d68a23fbbc804e35ec13e5a71cf7b56efa
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 20:56:22 +0700

    tambah komponen kartu informasi pelanggan

---
+++ cms/src/components/payments/customer-information-card.tsx
@@ -0,0 +1,11 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+// komponen kartu informasi pelanggan
+export default function CustomerInformationCard() {
+  return ()
+}
