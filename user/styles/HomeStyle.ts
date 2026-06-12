a225f76df915774d8f25f6c58618b2867767bcdb
Oka Windana Saputra <okwan2004@gmail.com>
2026-06-12 09:29:47 +0700

    mengatur tampilan container dan header

--- user/styles/HomeStyle.ts
+++ user/styles/HomeStyle.ts
@@ -1,3 +1,15 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({\
+    container: {
+    flex: 1,
+    backgroundColor: "#F4F5F0",
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    marginTop: 55,
+  },
+});
