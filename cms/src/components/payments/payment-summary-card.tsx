b6fd1478ce045c9a80854066d299da89a9603421
Rivaldzz <rivaldosborn@gmail.com>
2026-06-12 21:03:19 +0700

    komponen kartu ringkasan pembayaran

---
+++ cms/src/components/payments/payment-summary-card.tsx
@@ -0,0 +1,11 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+// komponen kartu ringkasan pembayaran
+export default function PaymentSummaryCard() {
+  return ()
+}
