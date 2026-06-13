fc631a43d70e685ed2ca299d39ab5ce0f1dde4f8
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-06-13 23:18:15 +0700

    feat(pendaftaran): mengimplementasikan PendaftaranController dengan metode CRUD

--- api/gateway/src/pendaftaran/pendaftaran.controller.ts
+++ api/gateway/src/pendaftaran/pendaftaran.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+
+import { PendaftaranService } from './pendaftaran.service';
+import { CreatePendaftaranDto } from './dto/create-pendaftaran.dto';
+
+import { JwtAccessGuard } from '../auth/guards/jwt-access.guard';
 
 @Controller('pendaftaran')
-export class PendaftaranController {}
+@UseGuards(JwtAccessGuard)
+export class PendaftaranController {
+  constructor(private readonly pendaftaranService: PendaftaranService) {}
+
+  @Post()
+  create(
+    @Body()
+    createPendaftaranDto: CreatePendaftaranDto,
+  ) {
+    return this.pendaftaranService.create(createPendaftaranDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.pendaftaranService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.pendaftaranService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.pendaftaranService.remove(id);
+  }
+}
