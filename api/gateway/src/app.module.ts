d929a28824918bb3f83175b671039b812b676710
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-06-13 22:54:22 +0700

    feat(pendaftaran): tambahkan PendaftaranModule ke dalam AppModule

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JalurModule } from './jalur/jalur.module';
 import { AuthModule } from './auth/auth.module';
+import { PendaftaranModule } from './pendaftaran/pendaftaran.module';
 
 @Module({
-  imports: [JalurModule, AuthModule],
+  imports: [JalurModule, AuthModule, PendaftaranModule],
   controllers: [AppController],
   providers: [AppService],
 })
