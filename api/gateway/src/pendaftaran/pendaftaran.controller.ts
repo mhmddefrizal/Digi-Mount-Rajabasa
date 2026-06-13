7ad57da43bb4681a4d14b976c5fd55a959ae29b7
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-06-13 23:56:24 +0700

    feat(pendaftaran): add update endpoint to modify pendaftaran records

--- api/gateway/src/pendaftaran/pendaftaran.controller.ts
+++ api/gateway/src/pendaftaran/pendaftaran.controller.ts
@@ -6,12 +6,14 @@ import {
   Param,
   Delete,
   UseGuards,
+  Patch,
 } from '@nestjs/common';
 
 import { PendaftaranService } from './pendaftaran.service';
 import { CreatePendaftaranDto } from './dto/create-pendaftaran.dto';
 
 import { JwtAccessGuard } from '../auth/guards/jwt-access.guard';
+import { UpdatePendaftaranDto } from './dto/update-pendaftaran.dto';
 
 @Controller('pendaftaran')
 @UseGuards(JwtAccessGuard)
@@ -34,6 +36,15 @@ export class PendaftaranController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.pendaftaranService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body()
+    updatePendaftaranDto: UpdatePendaftaranDto,
+  ) {
+    return this.pendaftaranService.update(id, updatePendaftaranDto);
   }
 
   @Delete(':id')
