02643131c682fa7bbb9493a1f0935b1a1e32c0fd
Oka Windana Saputra <okwan2004@gmail.com>
2026-06-13 17:25:55 +0700

    membuat halaman tambah  anggota kelompok

--- user/app/(home)/registrasi/anggota-kelompok.tsx
+++ user/app/(home)/registrasi/anggota-kelompok.tsx
@@ -10,3 +10,16 @@ import {
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useRouter } from "expo-router";
 import styles from "@/styles/AnggotaKelompokStyle";
+
+export default function AnggotaKelompok() {
+  const router = useRouter();
+
+  const [nama, setNama] = useState("");
+  const [nik, setNik] = useState("");
+  const [telepon, setTelepon] = useState("");
+
+  const [headlamp, setHeadlamp] = useState(false);
+  const [jasHujan, setJasHujan] = useState(false);
+
+  return ();
+}
