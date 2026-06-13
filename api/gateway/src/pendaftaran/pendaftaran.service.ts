703eba19af52e17e9e360bf2804e484fb38db20a
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-06-13 23:11:45 +0700

    feat(pendaftaran): tambahkan interface Pendaftaran untuk mendefinisikan struktur data pendaftaran

--- api/gateway/src/pendaftaran/pendaftaran.service.ts
+++ api/gateway/src/pendaftaran/pendaftaran.service.ts
@@ -2,6 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { pendaftaran_api } from '../common/instances/pendaftaran.instance';
 import { CreatePendaftaranDto } from './dto/create-pendaftaran.dto';
 
+export interface Pendaftaran {
+  id: string;
+  namaKetua: string;
+  nomorHp: string;
+  tipePendakian: string;
+  tanggalPendakian: Date;
+  jumlahAnggota: number;
+  jumlahMotor: number;
+  totalBiaya: number;
+  statusPembayaran: string;
+  jalurId: string;
+  createdAt: Date;
+}
 @Injectable()
 export class PendaftaranService {
   async create(createPendaftaranDto: CreatePendaftaranDto) {
