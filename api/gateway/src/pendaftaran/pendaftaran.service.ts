e858aba1daa26a594b8bf583c1ca194f3a7cef81
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-06-13 23:06:47 +0700

    feat(pendaftaran): mengimplementasikan metode create di PendaftaranService untuk pengajuan data registrasi

--- api/gateway/src/pendaftaran/pendaftaran.service.ts
+++ api/gateway/src/pendaftaran/pendaftaran.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { pendaftaran_api } from '../common/instances/pendaftaran.instance';
+import { CreatePendaftaranDto } from './dto/create-pendaftaran.dto';
 
 @Injectable()
-export class PendaftaranService {}
+export class PendaftaranService {
+  async create(createPendaftaranDto: CreatePendaftaranDto) {
+    const response = await pendaftaran_api.post('/', createPendaftaranDto);
+
+    return response.data;
+  }
+}
