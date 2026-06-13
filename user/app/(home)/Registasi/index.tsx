1eaed076468b77b974e2cb917c88fd2d1db49448
Oka Windana Saputra <okwan2004@gmail.com>
2026-06-13 13:12:31 +0700

    membuat halaman registrasi pendakian

---
+++ user/app/(home)/Registasi/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import styles from "@/styles/RegistrasiPendakianStyle";
+
+export default function RegistrasiPendakian() {
+  const [tanggal, setTanggal] = useState("");
+}
