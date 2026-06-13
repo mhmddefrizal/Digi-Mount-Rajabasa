507290d3aa2e45cb2213de1efb7519ace74e8c89
Oka Windana Saputra <okwan2004@gmail.com>
2026-06-13 13:17:07 +0700

    menambahkan header halaman registrasi

--- user/app/(home)/Registasi/index.tsx
+++ user/app/(home)/Registasi/index.tsx
@@ -11,4 +11,19 @@ import styles from "@/styles/RegistrasiPendakianStyle";
 
 export default function RegistrasiPendakian() {
   const [tanggal, setTanggal] = useState("");
+   return (
+    <ScrollView style={styles.container}>
+      
+      {/* Header */}
+      <View style={styles.header}>
+        <Ionicons
+          name="arrow-back"
+          size={22}
+          color="#1F3D36"
+        />
+
+        <Text style={styles.headerTitle}>
+          Digi-Mount Rajabasa
+        </Text>
+      </View>
 }
