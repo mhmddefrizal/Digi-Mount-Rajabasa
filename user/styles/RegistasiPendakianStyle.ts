9e4a83b36c073bd277de8093da93d898e3de2229
Oka Windana Saputra <okwan2004@gmail.com>
2026-06-13 13:14:29 +0700

    menambahkan styling container dan header

---
+++ user/styles/RegistasiPendakianStyle.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F0",
+    padding: 15,
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 40,
+  },
