72098c52da43794270dbfe6f9b6a95b433cbab04
Oka Windana Saputra <okwan2004@gmail.com>
2026-06-13 17:28:14 +0700

    menambahkan styling dasar halaman anggota kelompok

---
+++ user/styles/AnggotaKelompokStyle.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F6F2",
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingTop: 55,
+    paddingBottom: 15,
+    backgroundColor: "#FFF",
+  },
+
+  headerTitle: {
+    marginLeft: 15,
+    fontWeight: "600",
+    color: "#1F3D36",
+  },
